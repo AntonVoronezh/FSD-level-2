d4aedd5a4704161ea966776f56e9162a58c2cf2b
AntonVoronezh <pstut@mail.ru>
2019-07-19 15:50:57 +0300

    add responsive for container

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,4 +1,6 @@
 import "../../views/common/footer/footer";
 import "../../views/common/header/header";
 import "../../views/common/main/main";
+import "../../views/common/aside/aside";
+import "../../views/common/maincontent/maincontent";
 console.log("demo.js loaded");
