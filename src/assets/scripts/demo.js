88c901e13780381c0cce44a1914a4fb52a02f5b5
AntonVoronezh <pstut@mail.ru>
2019-07-19 22:36:11 +0300

    add top-nav

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -4,4 +4,5 @@ import '../../views/common/main/main';
 import '../../views/common/aside/aside';
 import '../../views/common/maincontent/maincontent';
 import '../../views/common/logo/logo';
+import '../../views/common/header-nav/header-nav';
 console.log('demo.js loaded');
