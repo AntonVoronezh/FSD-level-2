c00d7a8086088e0bc98d96c900fd83c5549a162a
AntonVoronezh <pstut@mail.ru>
2019-07-19 16:16:51 +0300

    add logo block

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -1,6 +1,7 @@
-import "../../views/common/footer/footer";
-import "../../views/common/header/header";
-import "../../views/common/main/main";
-import "../../views/common/aside/aside";
-import "../../views/common/maincontent/maincontent";
-console.log("demo.js loaded");
+import '../../views/common/footer/footer';
+import '../../views/common/header/header';
+import '../../views/common/main/main';
+import '../../views/common/aside/aside';
+import '../../views/common/maincontent/maincontent';
+import '../../views/common/logo/logo';
+console.log('demo.js loaded');
