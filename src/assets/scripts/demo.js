465d4ffd8869607e976f9d634b9006f75265bd6e
AntonVoronezh <pstut@mail.ru>
2019-07-26 00:53:48 +0300

    finish range slider

--- src/assets/scripts/demo.js
+++ src/assets/scripts/demo.js
@@ -16,30 +16,21 @@ import '../../views/common/btn/btn';
 import '../../views/common/header-nav-mobile/header-nav-mobile';
 import '../../views/common/pagination/pagination';
 import '../../views/common/range-slider/range-slider';
-// require ('ionRangeSlider')
-
-// $(".js-range-slider").ionRangeSlider({
-//     type: "double",
-//     min: 0,
-//     max: 1000,
-//     from: 200,
-//     to: 500,
-//     grid: true
-// });
-
-// $( ".js-range-slider" ).height(400);
-// $( ".js-range-slider" ).slider();
 $( ".js-range-slider" ).slider({
     range: true,
     min: 0,
     max: 15000,
     values: [ 5000, 10000 ],
     slide: function( event, ui ) {
-      $( ".range__amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      $( ".range__amount" ).val(ui.values[ 0 ] + "P - " + ui.values[ 1 ]  + "P");
     }
   });
 
   $( ".range__amount" ).val( $( ".js-range-slider" ).slider( "values", 0 ) +
     "P - " + $( ".js-range-slider" ).slider( "values", 1 ) + "P" );
+
+    $(function() {
+        $("#slider").slider();
+      });
 
 console.log('demo.js loadedd');
